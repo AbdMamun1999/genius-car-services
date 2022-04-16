b7541471162e26667c2464b2377cbff349283a0b
AbdMamun1999 <mamunrox@gmail.com>
2022-04-16 17:44:23 +0600

    added services and responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Services from './Pages/Home/Services/Services';
+import Login from './Pages/Login/Login';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Services></Services>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
