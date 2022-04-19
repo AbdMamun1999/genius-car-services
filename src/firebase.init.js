6753ec6eb68db7df78145ce53f4fc4d239881e70
AbdMamun1999 <mamunrox@gmail.com>
2022-04-19 13:10:41 +0600

    add environment set up

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKElJD-ApuEQOEg5LZ8uzXxpJJuyBoofE",
-  authDomain: "genius-car-services-2d7fd.firebaseapp.com",
-  projectId: "genius-car-services-2d7fd",
-  storageBucket: "genius-car-services-2d7fd.appspot.com",
-  messagingSenderId: "949915704202",
-  appId: "1:949915704202:web:a51c58e87ad133486d0673"
+  apiKey:process.env.REACT_App_apiKey,
+  authDomain:process.env.REACT_App_authDomain,
+  projectId:process.env.REACT_App_projectId,
+  storageBucket:process.env.REACT_App_storageBucket,
+  messagingSenderId:process.env.REACT_App_messagingSenderId,
+  appId:process.env.REACT_App_appId,
 };
 
 // Initialize Firebase
